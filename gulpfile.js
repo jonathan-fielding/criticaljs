1630baf8ca2340a24d24ccb158c748389d9a6140
Jonathan Fielding <jonathan@beamly.com>
2015-04-24 20:00:38 +0100

    setup gulp minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
 
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
+
+    return gulp.src('./src/criticaljs.js')
+            .pipe(uglify())
+            .pipe(header(banner, { pkg : pkg }))
+            .pipe(rename('criticaljs.min.js'))
+            .pipe(gulp.dest('./dist/'));
 });
